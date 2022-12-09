d2840aad4e108ca436809b4f068ee00672918b5a
Pedro A. González <pgonzalez@post.com>
2022-12-08 23:03:39 -0500

    Add basic urlDatabase and get json endpoint

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const urlDatabase = {
+  q2w3e4: 'https://www.lighthouselabs.ca',
+  a1s2d3: 'https://www.google.com',
+};
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/urls.json', (req, res) => {
+  res.send(urlDatabase);
 });
 
 app.listen(port, () => {
