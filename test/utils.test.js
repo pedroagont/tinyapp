e4486b7e66daa1124e7bdb93646f7b7864501bae
Pedro A. González <pgonzalez@post.com>
2022-12-11 02:03:17 -0500

    Add users db and getUserByEmail utils test

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,5 +1,20 @@
 const should = require('chai').should();
-const { generateNewId } = require('../utils');
+const { generateNewId, getUserByEmail } = require('../utils');
+
+const db = {
+  users: {
+    zxc135: {
+      id: 'zxc135',
+      email: 'user1@test.com',
+      password: '123',
+    },
+    cvb246: {
+      id: 'cvb246',
+      email: 'user2@test.com',
+      password: '123',
+    },
+  },
+};
 
 describe('utils', () => {
   describe('#generateNewId()', () => {
@@ -16,6 +31,25 @@ describe('utils', () => {
       const newId10chars = generateNewId(10);
       newId10chars.should.be.a('string');
       newId10chars.should.have.lengthOf(10);
+    });
+  });
+
+  describe('#getUserByEmail()', () => {
+    it('should return an object with properties id, email and password', () => {
+      const user = getUserByEmail('user1@test.com', db['users']);
+
+      user.should.be.a('object');
+      user.should.have.property('id');
+      user.should.have.property('email');
+      user.should.have.property('password');
+
+      user.id.should.be.a('string');
+      user.email.should.be.a('string');
+      user.password.should.be.a('string');
+    });
+    it('should return undefined when user is not found', () => {
+      const user = getUserByEmail('bad-email@test.com', db['users']);
+      should.not.exist(user);
     });
   });
 });
